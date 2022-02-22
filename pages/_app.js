eb241d781353d372ef159eccd5332f45bfd11612
song-code21 <wkcjf131313@gmail.com>
2022-02-22 22:53:33 +0900

    recap

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import NavBar from "../components/Navbar";
 
 // 말그대로 전역적인 무언가를 처리하고 싶을 때 _app.js를 추가한다면 next.js는 index를 렌더링하기 전에 _app.js를 처리하게 됨
+//_app.js에서는 ComponentProps를 받아오고 그 컴포넌트를 렌더링 해준다.
 export default function App({ Component, pageProps }) {
   return (
     <>
